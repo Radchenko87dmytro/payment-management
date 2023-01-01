2a58a2613562cdcd77351172570f3aed7331b95d
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2023-01-01 20:07:49 +0100

    Refactoring of code by adding component Costs.js and change props properties.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CostItem from "./components/CostItem";
+import Costs from "./components/Costs";
 
 
 
@@ -30,9 +31,10 @@ const costs = [
   return (
     <div>
       <h1>Lets lern React!</h1>
-      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}></CostItem>
-      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}></CostItem>
-      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}></CostItem>
+      <Costs costs={costs}/>
+      {/* <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/> 
+      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
+      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>*/}
     </div>
 
   );
