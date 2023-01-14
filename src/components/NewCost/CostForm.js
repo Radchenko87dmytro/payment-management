30bc164965de437741aebe6be2a4fb4c0d1770e3
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2023-01-14 23:31:15 +0100

    Refactoring CostForm.js file. It was added functions that set emty value in input form after pressing button

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -4,61 +4,88 @@ import "./CostForm.css"
 const CostForm = () => {
     // document.getElementById().addEventListener('Click', (event) => {})
 
-    // const [name, setName] = useState('');
-    // const [amount, setAmount] = useState('');
-    // const [date, setDate] = useState('');
+    const [inputName, setInputName] = useState("");
+    const [inputAmount, setInputAmount] = useState("");
+    const [inputDate, setInputDate] = useState("");
 
-    const [userInput, setUserInput] = useState({
-        name: '',
-        amount: '',
-        date: '',
-    });
+    // const [userInput, setUserInput] = useState({
+    //     name: '',
+    //     amount: '',
+    //     date: '',
+    // });
 
 
     const nameChangeHandler = (event) => {
-        // setName(event.target.value);
-        setUserInput({
-            ...userInput, //save inf from object userInput
-            name: event.target.value 
-        });
+        setInputName(event.target.value);
+        console.log(inputName);
+        // setUserInput({
+        //     ...userInput, //save inf from object userInput
+        //     name: event.target.value 
+        // });
+        // setUserInput((previousState) => { // previousState is automatically function that save previous state and render real state after changing
+        //     return {
+        //         ...previousState,
+        //         name: event.target.value
+        //     }
+        // })
     };
 
     const amountChangeHandler = (event) => {
-        //setAmount(event.target.value);
-        setUserInput({
-            ...userInput, //save inf from object userInput
-            amount: event.target.value 
-        });
+        setInputAmount(event.target.value);
+        //console.log(amount);
+        // setUserInput({
+        //     ...userInput, //save inf from object userInput
+        //     amount: event.target.value 
+        // });
     };
 
     const dateChangeHandler = (event) => {
-        // setDate(event.target.value);
-        setUserInput({
-            ...userInput, //save inf from object userInput
-            date: event.target.value 
-        });
+        setInputDate(event.target.value);
+        // setUserInput({
+        //     ...userInput, //save inf from object userInput
+        //     date: event.target.value 
+        // });
     };
 
-    return <form>
-        <div className="new-cost__controls">
-            <div className="new-cost__control">
-                <label>Name</label>
-                <input type='text' onChange={nameChangeHandler}/>
-            </div>
-            <div className="new-cost__control">
-                <label>Amount</label>
-                <input type = 'number' min = '0.01' step = '0.01' onChange={amountChangeHandler}/>
-            </div>
-            <div className="new-cost__control">
-                <label>Date</label>
-                <input type = 'date' min = '2019-01-01' step = '2023-12-31' onChange={dateChangeHandler}/>
-            </div>
-            <div className="new-cost__actions">
-                <button tabIndex="submit">Add payment</button>
-            </div>
-        </div>
+    const submitHandler = (event) => {
+        event.preventDefault(); // this method prevents action - request to the server
 
-    </form>
+        const costDate = {
+            name: inputName,
+            amount: inputAmount, 
+            date: new Date(inputDate)
+        };
+
+        console.log(costDate);
+        setInputName('');  // set emty value after pressing button
+        setInputAmount('');
+        setInputDate('');
+    };
+
+
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-cost__controls">
+                <div className="new-cost__control">
+                    <label>Name</label>
+                    <input type='text' value={inputName} onChange={nameChangeHandler}/>
+                </div>
+                <div className="new-cost__control">
+                    <label>Amount</label>
+                    <input type = 'number' value={inputAmount} min = '0.01' step = '0.01' onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-cost__control">
+                    <label>Date</label>
+                    <input  type = 'date' value={inputDate} min = '2019-01-01' step = '2023-12-31' onChange={dateChangeHandler}/>
+                </div>
+                <div className="new-cost__actions">
+                    <button type="submit">Add payment</button>
+                </div>
+            </div>
+
+        </form>
+        );
 }
 
 export default CostForm;
