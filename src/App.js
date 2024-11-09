9fceda9386e23322ee104748fcc9c80e4caedaa6
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-11-09 10:30:56 +0100

    hotfix: deleted old commits

--- src/App.js
+++ src/App.js
@@ -3,73 +3,64 @@ import React, { useState } from "react";
 import Costs from "./components/Costs/Costs";
 import NewCost from "./components/NewCost/NewCost";
 
-
 const INITIAL_COSTS = [
   {
-    id: 'c1',
+    id: "c1",
     date: new Date(2021, 2, 12),
-    description: 'Fridge',
+    description: "Fridge",
     amount: 900,
   },
   {
-    id: 'c2',
+    id: "c2",
     date: new Date(2022, 11, 1),
-    description: 'Bike',
+    description: "Bike",
     amount: 450,
   },
   {
-    id: 'c3',
+    id: "c3",
     date: new Date(2022, 11, 25),
-    description: 'jacket',
+    description: "jacket",
     amount: 380,
-  }
-]
+  },
+];
 
 // const App = () =>
 const App = () => {
+  //const p = document.createElement('p');
+  //p.textContent = 'Hey there!'
+  //document.getElementById('root').append(p);
 
-//const p = document.createElement('p');
-//p.textContent = 'Hey there!'
-//document.getElementById('root').append(p);
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
+  // return React.createElement('div', {},
+  //        React.createElement('h1', {}, "Lets lern React!"),
+  //        React.createElement(Costs, {costs: costs})
+  //        );
 
+  const addCostHandler = (cost) => {
+    setCosts((prevCosts) => {
+      // argument prevCosts we get automativ from react
 
-const [costs, setCosts] = useState(INITIAL_COSTS);
+      return [cost, ...prevCosts]; //we are set new element (cost,) and adding all elements from previous array by helping operator spred (...prevCosts)
+    });
 
+    // console.log(INITIAL_COSTS);
+    // console.log('App Componemt');
+  };
 
-// return React.createElement('div', {}, 
-//        React.createElement('h1', {}, "Lets lern React!"),
-//        React.createElement(Costs, {costs: costs})
-//        );
-
-const addCostHandler = (cost) => {
-   setCosts(prevCosts => { // argument prevCosts we get automativ from react
-    
-    return [cost, ...prevCosts] //we are set new element (cost,) and adding all elements from previous array by helping operator spred (...prevCosts)
-   }); 
-  
-  // console.log(INITIAL_COSTS);
-  // console.log('App Componemt');
-}
-
-const filterHandler = (selectedYear) => {
-  console.log(selectedYear);
-  // cost = cost.filter(item =>{
-  //   cost.conteins(selectedYear);
-  // })
-}
+  const filterHandler = (selectedYear) => {
+    console.log(selectedYear);
+    // cost = cost.filter(item =>{
+    //   cost.conteins(selectedYear);
+    // })
+  };
 
   return (
     <div>
-      <NewCost onAddCost={addCostHandler}/>
-      <Costs costs={costs} onFilter={filterHandler}/>
-
-      {/* <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/> 
-      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
-      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>*/}
+      <NewCost onAddCost={addCostHandler} />
+      <Costs costs={costs} onFilter={filterHandler} />
     </div>
-
   );
-}
+};
 
 export default App;
